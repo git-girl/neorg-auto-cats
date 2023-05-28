c2cd8c9d4ca8c5464fb3155f50a2d17d26ee9d26
git_girl <lotta.stefanova@posteo.de>
2023-05-28 15:40:25 +0200

    works for new files

--- lua/neorg/modules/external/auto-cats/module.lua
+++ lua/neorg/modules/external/auto-cats/module.lua
@@ -18,8 +18,8 @@ function module.setup()
   return { 
     success = true,
     requires = { 
-      "core.keybinds",
-      "core.dirman" 
+      'core.esupports.metagen',
+      'core.dirman'
     }
   }
 end
@@ -27,43 +27,89 @@ end
 module.private = { 
   enabled = true,
 
-  say_hello = function()
-    print("saying hello :)")
+  -- returns true or false
+  get_existing_metadata = function(buffer) 
+    return module.required["core.esupports.metagen"].is_metadata_present(buffer)
   end,
 
-  get_existing_metadata = function() 
+  remove_file_name_from_path = function(path) 
+    -- local fileName = path:match("/([^/]+)$")
+    -- if fileName then
+    --   trimmedPath = trimmedPath:gsub("/" .. fileName, "")
+    -- end
+    -- return trimmedPath
   end,
 
-  get_path_as_table = function() 
+  cut_path_before_workspace = function(path, workspace) 
+    index = string.find(path, workspace)
+    path = string.sub(path, index)
+    return path
+  end,
+
+  get_categories = function(path) 
+    workspace =  module.required["core.dirman"].get_workspace_match()
+
+    path = module.private.cut_path_before_workspace(path, workspace)
+    path = string.gsub(path, "/", " ")
+    -- remove everything after the last space
+    categories = string.gsub(path, "%s[^%s]*$", "")
+
+    return categories
+  end,
+
+  -- NOTE: I think this needs to behave differently based on metadata_exists
+  set_categories = function(constructed_metadata, categories) 
+    for index, element in ipairs(constructed_metadata) do
+      if element:match("^categories:") then
+        constructed_metadata[index] = "categories: " .. categories
+      end
+    end 
+    return constructed_metadata
+  end,
+
+  update_categories = function(constructed_metadata, categories) 
+  end,
+    -- if categoriesIndex then
+    --   local currentCategories = constructed_metadata[categoriesIndex]
+    --   print(categories)
+    --   local newCategories = currentCategories .. categories
+    --
+    --   constructed_metadata[categoriesIndex] = newCategories
+    -- end
+  main = function(buffer, path) 
+    metadata_exists, data = module.private.get_existing_metadata(buffer)
+    categories = module.private.get_categories(path)
+
+    if not metadata_exists then
+      constructed_metadata = module.required["core.esupports.metagen"].construct_metadata(buffer)
+      constructed_metadata = module.private.set_categories(constructed_metadata, categories)
+
+      vim.api.nvim_buf_set_lines(buffer, data.range[1], data.range[2], false, constructed_metadata)
+    else  
+      -- i would say it should update it regardeless but i think this goes against the 
+      -- neorg design of the metagen module having to be used explicitly and not overwriting so aggressively
+      -- module.required["core.esuppports.metagen"]
+
+      -- get the meta data that exists  
+      -- categories = already contained categories + any categories in path that arent contained already
+      -- set_categories
+    end
   end,
 }
 
 function module.load()
-    -- TODO: check how to properly add the aucomand group
-    autocats_augroup = vim.api.nvim_create_augroup("NeorgAutoCats", { clear = false })
+  -- TODO: check how to properly add the aucomand group
+  autocats_augroup = vim.api.nvim_create_augroup("NeorgAutoCats", { clear = false })
 
-    vim.api.nvim_create_autocmd({"BufEnter"}, {
-        desc = "Inject Metadata into Neorg file if not there, use directories for categories",
-        pattern = {"*.norg"},
-        callback = function(ev)
-          module.private.say_hello()
-        end  
-    })
+  vim.api.nvim_create_autocmd({"BufEnter"}, {
+    desc = "Inject Metadata into Neorg file if not there, use directories for categories",
+    pattern = {"*.norg"},
+    callback = function(ev)
+      buffer = ev.buf
+      path = ev.file
+      module.private.main(buffer, path)
+    end  
+  })
 end
-
-
---[[
-Helpful stuff from the api wiki: 
-- core.dirman.utils.expand_path: akes a path like $name/my/location and 
-converts $name into the full path of the workspace called name.
-
-neorg modules attrs: 
-  
-  private 
-  config  
-  public  
-  events  
-
---]]
 
 return module
