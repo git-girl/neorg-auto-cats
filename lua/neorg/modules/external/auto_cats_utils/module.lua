ea81f91c85ed25452523ddd8e28b4774574a120a
git_girl <lotta.stefanova@posteo.de>
2023-08-19 13:57:48 +0200

    refactor(utils): set impl for categories done

--- lua/neorg/modules/external/auto_cats_utils/module.lua
+++ lua/neorg/modules/external/auto_cats_utils/module.lua
@@ -28,15 +28,109 @@ module.public = {
 	--- Returns a simple set in which you can store simple types
 	--- so things like tables will raise an error
 	Set = {
-		type = nil,
-
 		---@param array table # optional array of elements of 1 type param from which to init the set
 		---@return { type: string|nil , new: function, add: function, get: function, remove: function }
-		new = function(array, self)
+		new = function(array)
+			---@type {type: nil|string, data: table<boolean|string|number|integer, boolean>}
+			local self = {
+				type = nil,
+
+				data = {},
+			}
+
+			---@type string[]
+			local accepted_types = { "boolean", "string", "number", "integer" }
+
+			-- TODO:
+			-- where add needs to check element type again and account for
+			-- no type being initialized in the elseif type(array == "nil") case
+			-- this might benefit from extracting some things out of _check_against_set_type
+			self.add = function(elem)
+				local elem_type = type(elem)
+
+				if self.type == nil then
+					if elem_type ~= "nil" then
+						self.type = elem_type
+					else
+						error(
+							"Set.add: tried to add nil to empty Set without a type, pass one of "
+								.. table.concat(accepted_types, ", ")
+						)
+					end
+				end
+
+				if elem_type == self.type then
+					table.insert(self.data, elem)
+				else
+					error("Set.add: type of elem was: " .. elem_type .. " type of Set is: " .. self.type)
+				end
+
+				return self
+			end
+
+			self.includes = function(requested_elem)
+				for _, set_elem in ipairs(self.data) do
+					if set_elem == requested_elem then
+						return true
+					end
+				end
+				return false
+			end
+
+			---@param requested_elem boolean|string|number|integer # The element to remove
+			---@param error boolean # Default: true, Whether or not to raise an error if the element isn't included
+			self.remove = function(requested_elem, error)
+				local raise_error = true
+				if error == false then
+					raise_error = false
+				end
+
+				for _, set_elem in ipairs(self.data) do
+					if set_elem == requested_elem then
+						table.remove(self.data, requested_elem)
+						return self
+					end
+				end
+
+				if raise_error then
+					error(
+						"Set.remove: element: "
+							.. requested_elem
+							.. " wasn't found in: "
+							.. self.data
+							.. " pass error=true to remove to disable this error"
+					)
+				end
+			end
+
+			---@param elem_type string
+			local check_against_set_type = function(elem_type)
+				if self.type ~= nil then
+					if elem_type ~= self.type then
+						error(
+							"element in array was of type: " .. elem_type .. " where previous type was: " .. self.type
+						)
+					end
+				else -- set self.type
+					for _, accepted_type in pairs(accepted_types) do
+						if elem_type == accepted_type then
+							self.type = elem_type
+						end
+					end
+					if self.type == nil then
+						error(
+							"no accepted type was found in the passed array"
+								.. " accepted types are: "
+								.. table.concat(accepted_types, ", ")
+						)
+					end
+				end
+			end
+
 			if type(array) == "table" then -- check the type of each element against self
 				---@type string
 				for _, elem in pairs(array) do
-					self._check_against_set_type(type(elem))
+					check_against_set_type(type(elem))
 
 					self.add(elem)
 				end
@@ -45,45 +139,6 @@ module.public = {
 				return self
 			else
 				error("passed something of type " .. type(array) .. "to Set.new")
-			end
-		end,
-
-		-- TODO:
-		-- where add needs to check element type again and account for
-		-- no type being initialized in the elseif type(array == "nil") case
-        -- this might benefit from extracting some things out of _check_against_set_type 
-		add = function(elem) end,
-
-		includes = function(elem) end,
-
-		remove = function(elem) end,
-
-		-- TODO: fix this type annotation also can't nil be in a set? nahhh
-		--
-		---@type <boolean|string|number|integer boolean>
-		_data = {},
-
-		_accepted_types = { "boolean", "string", "number", "integer" },
-
-		---@param elem_type string
-		_check_against_set_type = function(elem_type, self)
-			if self.type ~= nil then
-				if elem_type ~= self.type then
-					error("element in array was of type: " .. elem_type .. "where previous type was: " .. self.type)
-				end
-			else -- set self.type
-				for _, accepted_type in pairs(self._accepted_types) do
-					if elem_type == accepted_type then
-						self.type = elem_type
-					end
-				end
-				if self.type == nil then
-					error(
-						"no accepted type was found in the passed array"
-							.. "accepted types are: "
-							.. self._accepted_types
-					)
-				end
 			end
 		end,
 	},
